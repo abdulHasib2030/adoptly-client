83c86d4fa265730005cef73120a023f4e6599b3c
Abdul Hasib <abdulhasib2030@gmail.com>
2025-01-19 16:57:58 +0600

    add donation campaign page

--- src/pages/PetListing/PetListing.jsx
+++ src/pages/PetListing/PetListing.jsx
@@ -32,14 +32,7 @@ console.log(pets);
         { value: 'others', label: 'others' },
     ];
 
-    const handleSearch = (e) =>{
-        // console.log(e.target.value);
-        setSearch(e.target.value)
-        
-        console.log(search, selectedOption);
-
-
-    }
+ 
     useEffect(()=>{
             refetch()
         }, [search])
@@ -63,7 +56,7 @@ console.log(pets);
                         </div>
 
                         <div class="relative w-full">
-                            <input type="search" onChange={(e)=> handleSearch(e)} id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-[#0E7A81] focus:border-[#0E7A81] dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-[#0E7A81]" placeholder="Search pet name" required />
+                            <input type="search" onChange={(e)=> setSearch(e.target.value)} id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-[#0E7A81] focus:border-[#0E7A81] dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-[#0E7A81]" placeholder="Search pet name" required />
                             <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-[#0E7A81] rounded-e-lg border border-[#0E7A81] hover:bg-[#0E7A81] focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-[#0E7A81] dark:hover:bg-[#0E7A81] dark:focus:ring-[#0E7A81]">
                                 <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
@@ -104,7 +97,7 @@ console.log(pets);
                             </div> */}
 
                                     {/* <button class="py-2 px-4 border-2 rounded-lg font-bold text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white " id='adopt-${element.petId}'  >Adopt</button> */}
-                                    <Link to={'/pet-details/1'}>
+                                    <Link to={`/pet-details/${pet._id}`}>
                                         <button class="py-2 px-4 border-2 rounded-lg font-bold text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white" >Details</button>
                                     </Link>
                                 </div>
