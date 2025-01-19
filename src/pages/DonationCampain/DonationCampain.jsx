5f0f122898f29299e071c2eb20bae4af8963477a
Abdul Hasib <abdulhasib2030@gmail.com>
2025-01-19 19:04:23 +0600

    add pet listing page category and search functionallity

--- src/pages/DonationCampain/DonationCampain.jsx
+++ src/pages/DonationCampain/DonationCampain.jsx
@@ -1,21 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useAxiosPublic from '../../hooks/useAxiosPublic';
+import Loading from '../../components/Utlies/Loading';
 
 const DonationCampain = () => {
+    const axiosPublic = useAxiosPublic()
+    const {refetch, data:donations=[], isLoading} = useQuery({
+        queryKey: ['donations'],
+        queryFn: async()=>{
+            const res = await axiosPublic.get('/donation')
+            return res.data;
+        }
+    })
+
     return (
         <div className='my-32'>
+            {
+                isLoading ? <Loading></Loading>:
              <div id="item-show" className='grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3 gap-8'>
-                    <div className='border-2 p-5 rounded-xl'>
+                {
+                    donations.map(donation =>
+                     <div className='border-2 p-5 rounded-xl'>
                         <div>
 
-                            <img src="" class="rounded-lg" alt="not available" />
+                            <img src={donation.image} class="rounded-lg" alt="not available" />
                             <h3 class="text-xl font-bold mt-4 mb-2"></h3>
+                            
                             <div class="flex gap-4 text-[#131313B3] items-center">
-                                <img src="./images/menu.png" class="w-4 h-4" alt="" />
-                                <p></p>
-                            </div>
-                            <div class="flex gap-4 text-[#131313B3] items-center">
-                                <i class="fa-solid fa-cake-candles w-4 h-4"></i>
+                                
                                 <p>Birth: </p>
                             </div>
                             <div class="flex gap-4 text-[#131313B3] items-center">
@@ -38,104 +51,13 @@ const DonationCampain = () => {
                            <button class="py-2 px-4 border-2 rounded-lg font-bold text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white" >Details</button>
                            </Link> 
                         </div>
-                    </div>
-                    <div className='border-2 p-5 rounded-xl'>
-                        <div>
-
-                            <img src="" class="rounded-lg" alt="not available" />
-                            <h3 class="text-xl font-bold mt-4 mb-2"></h3>
-                            <div class="flex gap-4 text-[#131313B3] items-center">
-                                <img src="./images/menu.png" class="w-4 h-4" alt="" />
-                                <p></p>
-                            </div>
-                            <div class="flex gap-4 text-[#131313B3] items-center">
-                                <i class="fa-solid fa-cake-candles w-4 h-4"></i>
-                                <p>Birth: </p>
-                            </div>
-                            <div class="flex gap-4 text-[#131313B3] items-center">
-                                <i class="fa-solid fa-venus"></i>
-                                <p>Gender: </p>
-                            </div>
-                            <div class="flex gap-4 text-[#131313B3] items-center">
-                                <i class="fa-solid fa-dollar-sign"></i>
-                                <p >Price:  </p>
-                            </div>
-                        </div>
-                        <hr />
-                        <div class="flex gap-4 mt-4">
-                            <div class="py-2 px-2 border-2 rounded-lg hover:border-[#0E7A81]" >
-                                <img src="./images/favorite.png" alt="" class=" w-5 h-5" />
-                            </div>
-
-                            <button class="py-2 px-4 border-2 rounded-lg font-bold text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white " id='adopt-${element.petId}'  >Adopt</button>
-                            <button class="py-2 px-4 border-2 rounded-lg font-bold text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white" >Details</button>
-                        </div>
-                    </div>
-                    <div className='border-2 p-5 rounded-xl'>
-                        <div>
-
-                            <img src="" class="rounded-lg" alt="not available" />
-                            <h3 class="text-xl font-bold mt-4 mb-2"></h3>
-                            <div class="flex gap-4 text-[#131313B3] items-center">
-                                <img src="./images/menu.png" class="w-4 h-4" alt="" />
-                                <p></p>
-                            </div>
-                            <div class="flex gap-4 text-[#131313B3] items-center">
-                                <i class="fa-solid fa-cake-candles w-4 h-4"></i>
-                                <p>Birth: </p>
-                            </div>
-                            <div class="flex gap-4 text-[#131313B3] items-center">
-                                <i class="fa-solid fa-venus"></i>
-                                <p>Gender: </p>
-                            </div>
-                            <div class="flex gap-4 text-[#131313B3] items-center">
-                                <i class="fa-solid fa-dollar-sign"></i>
-                                <p >Price:  </p>
-                            </div>
-                        </div>
-                        <hr />
-                        <div class="flex gap-4 mt-4">
-                            <div class="py-2 px-2 border-2 rounded-lg hover:border-[#0E7A81]" >
-                                <img src="./images/favorite.png" alt="" class=" w-5 h-5" />
-                            </div>
-
-                            <button class="py-2 px-4 border-2 rounded-lg font-bold text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white " id='adopt-${element.petId}'  >Adopt</button>
-                            <button class="py-2 px-4 border-2 rounded-lg font-bold text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white" >Details</button>
-                        </div>
-                    </div>
-                    <div className='border-2 p-5 rounded-xl'>
-                        <div>
-
-                            <img src="" class="rounded-lg" alt="not available" />
-                            <h3 class="text-xl font-bold mt-4 mb-2"></h3>
-                            <div class="flex gap-4 text-[#131313B3] items-center">
-                                <img src="./images/menu.png" class="w-4 h-4" alt="" />
-                                <p></p>
-                            </div>
-                            <div class="flex gap-4 text-[#131313B3] items-center">
-                                <i class="fa-solid fa-cake-candles w-4 h-4"></i>
-                                <p>Birth: </p>
-                            </div>
-                            <div class="flex gap-4 text-[#131313B3] items-center">
-                                <i class="fa-solid fa-venus"></i>
-                                <p>Gender: </p>
-                            </div>
-                            <div class="flex gap-4 text-[#131313B3] items-center">
-                                <i class="fa-solid fa-dollar-sign"></i>
-                                <p >Price:  </p>
-                            </div>
-                        </div>
-                        <hr />
-                        <div class="flex gap-4 mt-4">
-                            <div class="py-2 px-2 border-2 rounded-lg hover:border-[#0E7A81]" >
-                                <img src="./images/favorite.png" alt="" class=" w-5 h-5" />
-                            </div>
-
-                            <button class="py-2 px-4 border-2 rounded-lg font-bold text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white " id='adopt-${element.petId}'  >Adopt</button>
-                            <button class="py-2 px-4 border-2 rounded-lg font-bold text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white" >Details</button>
-                        </div>
-                    </div>
+                    </div>   
+                    )
+                }
+                    
+                    
                 </div>
+            }
         </div>
     );
 };
--- src/pages/PetDetails/PetDetails.jsx
+++ src/pages/PetDetails/PetDetails.jsx
@@ -1,70 +1,168 @@
-import React from 'react';
+import React, { useState } from 'react';
 import image from '../../assets/banner2.jpg'
 import { useQuery } from '@tanstack/react-query';
 import useAxiosPublic from '../../hooks/useAxiosPublic';
 import { useLocation } from 'react-router-dom';
 import Loading from '../../components/Utlies/Loading';
+import { Button, Modal } from "flowbite-react";
+import { Label, TextInput } from "flowbite-react";
+import useAuth from '../../hooks/useAuth';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import toast from 'react-hot-toast';
 
 const PetDetails = () => {
     const axiosPublic = useAxiosPublic()
+    const axiosSecure = useAxiosSecure()
+    const { user } = useAuth()
+    const [openModal, setOpenModal] = useState(false);
     const location = useLocation()
+    const [selectedPet, setSelectedPet] = useState(null);
     const path = location.pathname.split('/')[2]
     console.log(path);
-    const {refetch, data:pet, isLoading} = useQuery({
+    const { refetch, data: pet, isLoading } = useQuery({
         queryKey: ['pet'],
-        queryFn: async()=>{
+        queryFn: async () => {
             const res = await axiosPublic.get(`/pet/${path}`)
             return res.data;
         }
     })
- console.log(pet);
-    return (
+    let description = ""
+    isLoading ? description : description = pet[0].description;
+
+    const handleAdoptBtn = (pet) => {
+        setSelectedPet(pet)
+        setOpenModal(true)
+    }
+
+    const handleAdoptForm = async(e) => {
+        e.preventDefault()
+        const formData = new FormData(e.target);
+        const adoptionData = {
+            petId: selectedPet[0]._id,
+            petName: selectedPet[0].name,
+            petImage: selectedPet[0].image,
+            userName: user.displayName,
+            email: user.email,
+            phoneNumber: formData.get("phone"),
+            address: formData.get("address"),
+        };
+        console.log(adoptionData);
+       const res = await axiosSecure.post('/adopt', adoptionData)
+       if(res.data.acknowledged && res.data.insertedId){
+        toast.success("Successfully adopt request.")
+        setOpenModal(false)
+       }
         
+    }
+
+    return (
+
         <div className='mt-16'>
             {
-            isLoading ? <Loading></Loading>:
-            <div>
-                <div className='h-96 w-full mx-auto border-2'>
-                    <img src={pet[0].image} className='h-full w-full' alt="" />
-                </div>
-
-                <div className='mt-5 grid md:grid-cols-12 gap-4'>
-                    <div className="w-full md:col-span-8 p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-                        <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Name of pet <span className='font-semibold'>{pet[0].name}</span></h5>
-                        <div>
-                            <ul className='list-disc text-start grid grid-cols-2 gap-3'>
-                                <li>Age: {pet[0].age}</li>
-                                <li>Category: {pet[0].catgory}</li>
-                                <li>Location: {pet[0].location}</li>
-                                <li>Added date: {pet[0].date}</li>
-                                <li>Adopted: {pet[0].adopted? "True": "False"}</li>
-                                <li>User: {pet[0].user}</li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div className='md:col-span-4'>
-                        <div className=" p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <svg className="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z" />
-                            </svg>
-                            <a href="#">
-                                <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
-                            </a>
-                            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
-                            <a href="#" className="inline-flex font-medium items-center text-blue-600 hover:underline">
-                                See our guideline
-                                <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
-                                </svg>
-                            </a>
+                isLoading ? <Loading></Loading> :
+                    <div>
+                        <div className='h-96 w-full mx-auto border-2'>
+                            <img src={pet[0].image} className='h-full w-full' alt="" />
                         </div>
 
-                    </div>
+                        <div className='mt-5 grid md:grid-cols-12 gap-4'>
+                            <div className="w-full  md:col-span-8 space-y-2 p-4 text-start bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                                <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Name of pet <span className='font-semibold'>{pet[0].name}</span></h5>
+                                <div>
+                                    <ul className='list-disc text-start grid grid-cols-2 gap-3'>
+                                        <li>Age: {pet[0].age}</li>
+                                        <li>Category: {pet[0].catgory}</li>
+                                        <li>Location: {pet[0].location}</li>
+                                        <li>Added date: {pet[0].date}</li>
+                                        <li>Adopted: <span className=''>{pet[0].adopted ? "True" : "False"}</span></li>
+                                        <li>User: {pet[0].user}</li>
+                                    </ul>
+                                    <p className='text-start mt-2 font-bold'>Owner note: <span className='font-normal'>{pet[0].note_owner}</span></p>
+                                    <p className='font-bold'>Description:</p>
+                                    <div dangerouslySetInnerHTML={{ __html: description }} />
+                                </div>
+                            </div>
 
-                </div>
-            </div>
-        }
+                            <div className='md:col-span-4'>
+                                <div className=" p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                    <img src="https://img.icons8.com/?size=100&id=eGMiZMLt3CD6&format=png&color=000000" alt="" className='w-20 mx-auto mb-2' />
+                                    <button onClick={() => handleAdoptBtn(pet)} class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                                        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                            Adopted
+                                        </span>
+                                    </button>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+            }
+
+
+            {/* modal */}
+            {
+                selectedPet &&
+                <Modal show={openModal} onClose={() => setOpenModal(false)}>
+                    <Modal.Header>Adopt {selectedPet[0].name}</Modal.Header>
+                    <Modal.Body>
+                        <div className="space-y-6">
+                            <form onSubmit={handleAdoptForm}>
+                                <div className="space-y-4">
+                                    <div>
+                                        <Label htmlFor="userName" value="Your Name" />
+                                        <TextInput
+                                            id="userName"
+                                            value={user.displayName}
+                                            disabled
+                                            className="w-full"
+                                        />
+                                    </div>
+                                    <div>
+                                        <Label htmlFor="email" value="Your Email" />
+                                        <TextInput
+                                            id="email"
+                                            value={user.email}
+                                            disabled
+                                            className="w-full"
+                                        />
+                                    </div>
+                                    <div>
+                                        <Label htmlFor="phone" value="Phone Number*" />
+                                        <TextInput
+                                            id="phone"
+                                            name="phone"
+                                            type="number"
+                                            placeholder="Enter your phone number"
+                                            required
+                                            className="w-full"
+                                        />
+                                    </div>
+                                    <div>
+                                        <Label htmlFor="address" value="Address*" />
+                                        <TextInput
+                                            id="address"
+                                            name="address"
+                                            placeholder="Enter your address"
+                                            defaultValue={user.address}
+                                            required
+                                            className="w-full"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="mt-4 flex justify-end">
+                                    <Button type="submit">Submit</Button>
+                                </div>
+                            </form>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button color="gray" onClick={() => setOpenModal(false)}>
+                            Decline
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            }
         </div>
     );
 };
